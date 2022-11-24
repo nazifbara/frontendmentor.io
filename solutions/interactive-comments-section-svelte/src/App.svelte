ebb4a3a45c8dbdc599fa6293405bbeed13e8dbba
nazifbara <nazifbarassounon@gmail.com>
2022-11-24 17:34:13 +0100

    add the basic structure

--- solutions/interactive-comments-section-svelte/src/App.svelte
+++ solutions/interactive-comments-section-svelte/src/App.svelte
@@ -1,3 +1,23 @@
+<script>
+  import { onMount } from 'svelte'
+
+  import { currentUser, comments } from './data.json'
+  import Comment from './lib/Comment.svelte';
+</script>
+
 <main>
-  <h1>Interative Comments Section</h1>
+  <ul>
+    {#each comments as comment}
+    <li>
+      <Comment {comment} />
+    </li>
+  {/each}
+  </ul>
+  <form>
+    <textarea></textarea>
+    <div>
+      <img src="{currentUser.image.webp}" alt="Avatar of {currentUser.username}"/>
+      <button>send</button>
+    </div>
+  </form>
 </main>
---
+++ solutions/interactive-comments-section-svelte/src/lib/Comment.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let comment;
+</script>
+
+<article>
+  <header>
+    <img src={comment.user.image.webp} alt="Avatar of {comment.user.username}" />
+    <h1>{comment.user.username}</h1>
+    <time>{comment.createdAt}</time>
+  </header>
+  
+  <p>{comment.content}</p>
+
+  <div>
+    <div>
+      <button>
+        +
+      </button>
+      <span>{comment.score}</span>
+      <button>
+        -
+      </button>
+    </div>
+
+    <button>reply</button>
+  </div>
+</article>
+
+<ul>
+  {#each comment.replies ?? [] as reply }
+    <li>
+      <svelte:self comment={reply} />
+    </li>
+  {/each}
+</ul>
