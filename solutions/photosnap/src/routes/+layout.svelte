fed8c8cf0336657df58f81d27d932f15afe602e0
nazifbara <nazifbarassounon@gmail.com>
2023-02-07 17:44:08 +0100

    add DM Sans font

---
+++ solutions/photosnap/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<slot />
+
+<style lang="postcss">
+	:global(body) {
+		font-family: 'DM Sans', sans-serif;
+	}
+</style>
--- solutions/photosnap/src/routes/+page.svelte
+++ solutions/photosnap/src/routes/+page.svelte
@@ -1,5 +1,1 @@
-<script>
-	import '../app.css';
-</script>
-
 <h1>Photosnap</h1>
