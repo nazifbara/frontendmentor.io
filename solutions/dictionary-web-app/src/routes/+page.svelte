52145fcc013122c41e24f9ce0906fad122822206
nazifbara <nazifbarassounon@gmail.com>
2023-03-14 14:22:43 +0100

    add colors to the theme

--- solutions/dictionary-web-app/src/routes/+page.svelte
+++ solutions/dictionary-web-app/src/routes/+page.svelte
@@ -1,5 +1,7 @@
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error aliquid corporis eligendi aliquam
-voluptates harum nostrum id cum nisi. Illum dolorem numquam eius laborum non, repudiandae cumque,
-quod ducimus dolorum corrupti harum quibusdam perspiciatis vitae repellat accusamus dignissimos quam
-doloribus ipsam voluptatibus possimus assumenda explicabo consequatur reprehenderit. Natus,
-doloremque rerum?
+<p class="text-text">
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error aliquid corporis eligendi aliquam
+	voluptates harum nostrum id cum nisi. Illum dolorem numquam eius laborum non, repudiandae cumque,
+	quod ducimus dolorum corrupti harum quibusdam perspiciatis vitae repellat accusamus dignissimos
+	quam doloribus ipsam voluptatibus possimus assumenda explicabo consequatur reprehenderit. Natus,
+	doloremque rerum?
+</p>
--- solutions/dictionary-web-app/tailwind.config.cjs
+++ solutions/dictionary-web-app/tailwind.config.cjs
@@ -7,6 +7,18 @@ module.exports = {
       'serif': ['Lora'],
       'mono': ['Inconsolata'],
     },
+    colors: {
+      'body': '#FFF',
+      'body2': '#F4F4F4',
+      'text': '#2D2D2D',
+      'text2': '#757575',
+      'body-dark': '#050505',
+      'body2-dark': '#1F1F1F',
+      'text-dark': '#FFF',
+      'text2-dark': '#757575',
+      'primary': '#A445ED',
+      'danger': '#FF5252'
+    },
     extend: {},
   },
   plugins: [],
