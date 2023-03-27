58a0d235d29b8942f792a67b181fd455e92ade61
nazifbara <nazifbarassounon@gmail.com>
2023-03-27 13:23:01 +0100

    style the error page

--- solutions/dictionary-web-app/src/routes/[word]/+error.svelte
+++ solutions/dictionary-web-app/src/routes/[word]/+error.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import Text from '$lib/components/Text.svelte';
 
 	const title: { [any: number]: string } = {
 		404: 'No Definitions Found',
@@ -13,8 +14,11 @@
 	</title>
 </svelte:head>
 
-<h1>
-	{title[$page.status]}
-</h1>
+<section class="text-center mt-32">
+	<div class="text-[4rem] mb-4">{$page.status === 404 ? '😕' : '😵'}</div>
+	<h1 class="text-xl font-bold mb-6">
+		{title[$page.status]}
+	</h1>
 
-<p>{$page.error?.message}</p>
+	<Text element="p" color="text2">{$page.error?.message}</Text>
+</section>
